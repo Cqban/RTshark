23fec54dd52e20a9bb93e931c6a9c4faa37fe50b
Cqban <54151734+Cqban@users.noreply.github.com>
2022-11-15 17:38:40 +0100

    Started using thread module
    - now launching nmap scan as a thread
    - now launching wireshark capture as a thread
    
    Co-Authored-By: Cliff4537 <117033688+Cliff4537@users.noreply.github.com>

--- nomduprojet/script.py
+++ nomduprojet/script.py
@@ -1,29 +1,40 @@
 import pyshark
 import nmap
+import threading
 
-# scanner = nmap.PortScanner()
-# ip_addr = input("Adresse IP à scanner : ")
+scanner = nmap.PortScanner()
+ip_addr = input("Adresse IP à scanner : ")
+scanfinished = False
 
-capture = pyshark.LiveCapture(interface='Wi-Fi', display_filter='tcp.flags.syn==1 and tcp.flags.ack==1', output_file='prout.pcap')
-capture.sniff(timeout=20000)
-print(capture)
+def sniff(object):
+    object.sniff(timeout=0)
 
-# scanner.scan(ip_addr,'1-65535', '-v -sS')
-# print(scanner.scaninfo())
-# print("Etat de l'adresse IP: ", scanner[ip_addr].state())
-# print("Ports Ouverts: ", scanner[ip_addr]['tcp'].keys())
+#Capture wireshark avec filtre ciblant les segment ayant pour flag SYN et ACK (réponses signifiant qu'un port est ouvert suite au scan nmap)
+def capturewireshark():
+    capture = pyshark.LiveCapture(interface="Wi-Fi", bpf_filter='tcp[tcpflags] == tcp-syn|tcp-ack', output_file='./nomduprojet/capture.pcap')
+    threading.Thread(target=sniff,args=[capture]).start()
+    while scanfinished: # Quand scanfinished = True, stopper la capture
+        capture.close()
 
+# balayage des ports 1 à 1024 en mode TCP Scan de l'@ IP choisie
+def scannmap():
+    scanner.scan(ip_addr,'1-1024', '-v -sT') # équivalent de "nmap [ip] -p 1-1024 -v -sT"
+    print(scanner.scaninfo())
+    print("Etat de l'adresse IP: ", scanner[ip_addr].state()) # affiche si l'adresse IP accessible ou non
+    print("Ports Ouverts: ", scanner[ip_addr]['tcp'].keys()) # 
+    scanfinished = True
 
-# capturefile = pyshark.FileCapture("capture.pcap")
+threading.Thread(target=capturewireshark).start()
+threading.Thread(target=scannmap).start()
 
-# liste = []
-# for p in capturefile:
-#     if hasattr(p, 'tcp'):
-#         liste.append(p.tcp.srcport)
+# Traitement du fichier pcap
+capturefile = pyshark.FileCapture("capture.pcap")
 
-# with open('textfile.txt', 'w') as input:
-#     for listitem in sorted(set(liste)):
-#         input.write(f'{listitem}\n')
+liste = []
+for p in capturefile:
+    if hasattr(p, 'tcp'):
+        liste.append(p.tcp.srcport)
 
-
-
+with open('textfile.txt', 'w') as input:
+    for listitem in sorted(set(liste)):
+        input.write(f'{listitem}\n')
