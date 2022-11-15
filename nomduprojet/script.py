28a884322d71f1627f822009f893226d2e0e8577
Cqban <54151734+Cqban@users.noreply.github.com>
2022-11-15 13:28:13 +0100

    ee

--- nomduprojet/script.py
+++ nomduprojet/script.py
@@ -1,18 +1,29 @@
 import pyshark
+import nmap
 
-filename = input("Ecris le nom du fichier de capture dont tu veux extraire les ports source ouverts : ")
-capturefile = pyshark.FileCapture(filename)
+# scanner = nmap.PortScanner()
+# ip_addr = input("Adresse IP à scanner : ")
 
-liste = []
-for p in capturefile:
-    if hasattr(p, 'tcp'):
-        liste.append(p.tcp.srcport)
-    if hasattr(p, 'udp'):
-        liste.append(p.udp.srcport)
+capture = pyshark.LiveCapture(interface='Wi-Fi', display_filter='tcp.flags.syn==1 and tcp.flags.ack==1', output_file='prout.pcap')
+capture.sniff(timeout=20000)
+print(capture)
 
-with open('textfile.txt', 'w') as input:
-    for listitem in sorted(set(liste)):
-        input.write(f'{listitem}\n')
+# scanner.scan(ip_addr,'1-65535', '-v -sS')
+# print(scanner.scaninfo())
+# print("Etat de l'adresse IP: ", scanner[ip_addr].state())
+# print("Ports Ouverts: ", scanner[ip_addr]['tcp'].keys())
+
+
+# capturefile = pyshark.FileCapture("capture.pcap")
+
+# liste = []
+# for p in capturefile:
+#     if hasattr(p, 'tcp'):
+#         liste.append(p.tcp.srcport)
+
+# with open('textfile.txt', 'w') as input:
+#     for listitem in sorted(set(liste)):
+#         input.write(f'{listitem}\n')
 
 
 
