389eb4070ebcdc876af090692352162572e8ba8b
Cqban <54151734+Cqban@users.noreply.github.com>
2022-11-24 22:40:32 +0100

    first try to implement args

--- RTshark/script.py
+++ RTshark/script.py
@@ -1,13 +1,24 @@
 import pyshark
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()                                               
+parser.add_argument( "input-file", type=str )
+parser.add_argument( "output-file", type=str )
+args = parser.parse_args()
+
+input_file = args.input_file
+output_file = args.output_file
+
 
 # Traitement du fichier pcap
-capturefile = pyshark.FileCapture("capture.pcap")
+capturefile = pyshark.FileCapture(input_file)
 
 liste = []
 for p in capturefile:
     if hasattr(p, 'tcp'):
         liste.append(p.tcp.srcport)
 
-with open('textfile.txt', 'w') as input:
+with open(output_file, 'w') as input:
     for listitem in sorted(set(liste)):
         input.write(f'{listitem}\n')
