ed800f8348ede05aa6d0bd7c124ef7814205dead
Cqban <54151734+Cqban@users.noreply.github.com>
2023-01-09 20:51:40 +0100

    moved generate_web function to a module

--- RTshark/module1.py
+++ RTshark/module1.py
@@ -1,28 +1,73 @@
 """
-.. module:: module1
+.. module:: generate_webpage
    :platform: Unix, windows
    :synopsis: module1 du projet
 
-.. moduleauthor:: Nom Prenom <prenom.nom@etu.univ-poitiers.fr>
-
+.. moduleauthor:: Lechevrel Malko Fournier Vincent <malko.lechevrel@etu.univ-poitiers.fr>
 
 """
 
-def fonction1(param1, param2):
-    """ Cette fonction fait quelque chose.
-
-    :param param1: premier paramètre.
-    :type param1: str
-    :param param2: deuxième paramètre.
-    :type param2: bool
-    :returns: description de la variable retournée.
-    :rtype: int
-    :raises: TypeError
-    :example:
+# Function to generate an HTML page displaying the given list of ports
+def generate_webpage(port_list):
+    # Code CSS
+    codecss = """body{
+    background-image: url(https://i.postimg.cc/Nj375g1j/background.jpg);
+    }
+        .rslt {
+            display: flex;
+        justify-content: center;
+        align-items: center;
+        border:1px #C0C0C0;
+        border-style: solid;
+        border-radius: 5px;
+        background: black;
+    }
+        .rslt th {
+        border:1px black;
+        background:#F0F0F0;
+        border-style: groove;
+        border-radius: 10px;
+        font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+        font-size: 2rem;
+    }
+        .rslt td {
+        font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+        font-size: medium;
+        text-shadow: 3px white;
+        font-style: bold;
+        border:1px white;
+        color: white;
+        background: black;
+    }"""
     
-    .. code-block:: python
+    # Code HTML
+    codehtml = f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <style>
+        {codecss}
+    </style>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/style.css">
+        
+        <title>Open Ports</title>
+    </head>
+    <body>
+        <table class="rslt">
+            <tr>
+                <th>List of Open Ports</th>
+            </tr>
+            <tr>
+                <td>{port_list}</td>
+            </tr>
+    <tbody>
+        
+            </tbody>
+        </table>
+    </body>
+    </html>""" 
 
-     entier = fonction1("toto", True)
-
-    """
-    return 0
+    return codehtml
--- RTshark/rtshark.py
+++ RTshark/rtshark.py
@@ -1,6 +1,7 @@
 import pyshark
 import argparse
 import webbrowser
+from module1 import generate_webpage
 
 # Function to extract a list of unique port numbers from a pcap file
 def extract_ports(capture_file):
@@ -9,71 +10,6 @@ def extract_ports(capture_file):
         if hasattr(packet, 'tcp'):
             port_list.append(int(packet.tcp.srcport))
     return sorted(set(port_list))
-
-# Function to generate an HTML page displaying the given list of ports
-def generate_webpage(port_list):
-    # Code CSS
-    codecss = """body{
-    background-image: url(https://i.postimg.cc/Nj375g1j/background.jpg);
-    }
-        .rslt {
-            display: flex;
-        justify-content: center;
-        align-items: center;
-        border:1px #C0C0C0;
-        border-style: solid;
-        border-radius: 5px;
-        background: black;
-    }
-        .rslt th {
-        border:1px black;
-        background:#F0F0F0;
-        border-style: groove;
-        border-radius: 10px;
-        font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
-        font-size: 2rem;
-    }
-        .rslt td {
-        font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
-        font-size: medium;
-        text-shadow: 3px white;
-        font-style: bold;
-        border:1px white;
-        color: white;
-        background: black;
-    }"""
-    
-    # Code HTML
-    codehtml = f"""
-    <!DOCTYPE html>
-    <html lang="en">
-    <style>
-        {codecss}
-    </style>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/css/style.css">
-        
-        <title>Open Ports</title>
-    </head>
-    <body>
-        <table class="rslt">
-            <tr>
-                <th>List of Open Ports</th>
-            </tr>
-            <tr>
-                <td>{port_list}</td>
-            </tr>
-    <tbody>
-        
-            </tbody>
-        </table>
-    </body>
-    </html>""" 
-
-    return codehtml
 
 # Function to write the given HTML code to the specified file
 def write_page(html, output_file):
