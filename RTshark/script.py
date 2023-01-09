1b5210138c9fb9ade7134a37937ee3886637e929
Cqban <54151734+Cqban@users.noreply.github.com>
2023-01-09 20:30:51 +0100

    last version of script

--- RTshark/script.py
+++ RTshark/script.py
@@ -1,47 +1,100 @@
 import pyshark
-import sys
 import argparse
 import webbrowser
 
-parser = argparse.ArgumentParser()                                               
-parser.add_argument('--input-file', help="PCAP file to analyze", required=True)
-parser.add_argument('--output-file', help="Output file to write the HTML code", required=True)
-args = parser.parse_args()
+# Function to extract a list of unique port numbers from a pcap file
+def extract_ports(capture_file):
+    port_list = []
+    for packet in capture_file:
+        if hasattr(packet, 'tcp'):
+            port_list.append(int(packet.tcp.srcport))
+    return sorted(set(port_list))
 
-input_file = (args.input_file)
-output_file = (args.output_file)
-
-
-# Traitement du fichier pcap
-capturefile = pyshark.FileCapture(input_file)
-
-liste = []
-for p in capturefile:
-    if hasattr(p, 'tcp'):
-        liste.append(int(p.tcp.srcport))
-
-listev2 = sorted(set(liste))
-
-css = """body {
-        background-color: red;
+# Function to generate an HTML page displaying the given list of ports
+def generate_webpage(port_list):
+    # Code CSS
+    codecss = """body{
+    background-image: url(https://i.postimg.cc/Nj375g1j/background.jpg);
+    }
+        .rslt {
+            display: flex;
+        justify-content: center;
+        align-items: center;
+        border:1px #C0C0C0;
+        border-style: solid;
+        border-radius: 5px;
+        background: black;
+    }
+        .rslt th {
+        border:1px black;
+        background:#F0F0F0;
+        border-style: groove;
+        border-radius: 10px;
+        font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+        font-size: 2rem;
+    }
+        .rslt td {
+        font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+        font-size: medium;
+        text-shadow: 3px white;
+        font-style: bold;
+        border:1px white;
+        color: white;
+        background: black;
     }"""
-
-codehtml = f"""
-<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
-<!DOCTYPE html>
-<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\" lang=\"fr\" dir=\"ltr\">
-<style>
-    {css}
-</style>
-<head>
-<title> Mon titre </title>
-</head>
-<body>
-    Liste des ports ouverts : {listev2}
-</body>
-</html>"""
-
-with open(output_file, 'w') as writinghtml:
-    writinghtml.write(codehtml)
     
-webbrowser.open_new_tab(output_file)
+    # Code HTML
+    codehtml = f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <style>
+        {codecss}
+    </style>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/style.css">
+        
+        <title>Open Ports</title>
+    </head>
+    <body>
+        <table class="rslt">
+            <tr>
+                <th>List of Open Ports</th>
+            </tr>
+            <tr>
+                <td>{port_list}</td>
+            </tr>
+    <tbody>
+        
+            </tbody>
+        </table>
+    </body>
+    </html>""" 
+
+    return codehtml
+
+# Function to write the given HTML code to the specified file
+def write_page(html, output_file):
+    with open(output_file, 'w') as f:
+        f.write(html)
+
+# Main function which handles arguments and the processing of the other functions.
+def main():
+    parser = argparse.ArgumentParser()                                               
+    parser.add_argument('--input-file', help="PCAP file to analyze", required=True)
+    parser.add_argument('--output-file', help="Output file to write the HTML code", required=True)
+    args = parser.parse_args()
+
+    input_file = args.input_file
+    output_file = args.output_file
+
+    capture_file = pyshark.FileCapture(input_file)
+    port_list = extract_ports(capture_file)
+    html = generate_webpage(port_list)
+    write_page(html, output_file)
+    webbrowser.open_new_tab(output_file)
+
+if __name__ == "__main__":
+    main()
