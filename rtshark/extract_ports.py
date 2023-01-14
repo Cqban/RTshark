ddf641d86381d36dffa3e75bd3e31aa3da32f833
Cqban <54151734+Cqban@users.noreply.github.com>
2023-01-14 18:20:20 +0100

    Add files via upload

---
+++ rtshark/extract_ports.py
@@ -0,0 +1,30 @@
+"""
+.. module:: extract_ports
+   :platform: Unix, windows
+   :synopsis: Port extraction function
+
+.. moduleauthor:: Lechevrel Malko Fournier Vincent <malko.lechevrel@etu.univ-poitiers.fr>
+
+"""
+
+def extract_ports(capture_file):
+    """
+    This function allows you to extract open ports from a PCAP file.
+
+    :param args: PCAP file
+    :type args: str
+    :returns: Returns the open ports as a list.
+    :rtype: int
+    :raises: TypeError
+    :example: 
+
+    [80, 443, 25565]
+
+    """
+    
+    port_list = []
+    for packet in capture_file:
+        if hasattr(packet, 'tcp'):
+            if "0x012" in str(packet):
+                port_list.append(int(packet.tcp.srcport))
+    return sorted(set(port_list))
---
+++ rtshark/write_page.py
@@ -0,0 +1,24 @@
+"""
+.. module:: generate_webpage
+   :platform: Unix, windows
+   :synopsis: Writing HTML file function
+
+.. moduleauthor:: Lechevrel Malko Fournier Vincent <malko.lechevrel@etu.univ-poitiers.fr>
+
+"""
+
+def write_page(html, output_file):
+    """
+    This function write the content of the generate_webpage function inside of a HTML file.
+
+    :param args: Output file
+    :type args: str
+    :raises: TypeError
+    :example: 
+
+    index.html
+    
+    """
+
+    with open(output_file, 'w') as f:
+        f.write(html)
